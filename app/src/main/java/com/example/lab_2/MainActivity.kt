3a479e58285fffaa3e9ba6a121d2f8873b70f8c5
Jezaile <whocaresmymail@gmail.com>
2025-04-27 14:53:49 +0700

    Добавлен вывод полученного числа и сообщения о некорректности ввода по нажатию кнопки вычислить. Функция caclNumb исправлена, теперь выводит ближайшее меньшее значение, а не большее.

--- app/src/main/java/com/example/lab_2/MainActivity.kt
+++ app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.lab_2
 
 import android.os.Bundle
-
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -28,22 +26,33 @@ class MainActivity : AppCompatActivity() {
 
     fun calcNumb(a:Float): Float {
         var b = 0F
+        var prev_b = 0F
         var c = 0F
-        while (b <= a){
+        while (b < a) {
+            prev_b = b
             c += 1F
             b += 1/c
         }
-        return b
+        return prev_b
+
     }
 
     fun onClickButton (@Suppress("UNUSED_PARAMETER")view:View) {
         val n = findViewById<TextView>(R.id.TextInputEditText)
         a = n.text.toString().toFloatOrNull() ?: 7F
-        if (a >= 7F) {
+        val out = findViewById<TextView>(R.id.textView3)
+        val outL = findViewById<TextView>(R.id.textView2)
+        if (a >= 7F || a < 1) {
+            outL.text = "Введите корректное число"
+            outL.visibility = View.VISIBLE
+            out.visibility = View.GONE
             return
         }
         b  = calcNumb(a)
-        Log.d("numb", a.toString())
-        Log.d("numb", b.toString())
+        outL.text = "Ближайший меньший введеного числа \n" +
+                "член гармонического ряда"
+        outL.visibility = View.VISIBLE
+        out.text = b.toString()
+        out.visibility = View.VISIBLE
     }
 }
