e978b1045eda77aa83389f0863174b1f31f48a1d
Jezaile <whocaresmymail@gmail.com>
2025-04-27 14:18:40 +0700

    Добавлена функция onClickButton, по нажатию кнопки "Вычислить" принимающая введенное число и вызывающая функцию calcNumb если введенное число удовлетворяет заданным условием (<7).

--- app/src/main/java/com/example/lab_2/MainActivity.kt
+++ app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.lab_2
 import android.os.Bundle
 
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    var a:Float = 3.0F
+    var a:Float = 0F
     var b:Float = 0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,17 +24,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         b = calcNumb(a)
-        Log.d("numb", a.toString())
-        Log.d("numb", b.toString())
     }
 
     fun calcNumb(a:Float): Float {
         var b = 0F
         var c = 0F
-        while (b < a){
+        while (b <= a){
             c += 1F
             b += 1/c
         }
         return b
+    }
+
+    fun onClickButton (@Suppress("UNUSED_PARAMETER")view:View) {
+        val n = findViewById<TextView>(R.id.TextInputEditText)
+        a = n.text.toString().toFloatOrNull() ?: 7F
+        if (a >= 7F) {
+            return
+        }
+        b  = calcNumb(a)
+        Log.d("numb", a.toString())
+        Log.d("numb", b.toString())
     }
 }
