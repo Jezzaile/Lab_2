4b007ac47845f4f36dc8556de52a31fb55db0f9f
Jezaile <whocaresmymail@gmail.com>
2025-04-27 13:44:51 +0700

    Добавлена функция calcNumb, находящая в гармоническом ряду значение больше заданного (a) и возвращающая его.

--- app/src/main/java/com/example/lab_2/MainActivity.kt
+++ app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.lab_2
 
 import android.os.Bundle
+
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var a:Float = 3.0F
+    var b:Float = 0F
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        b = calcNumb(a)
+        Log.d("numb", a.toString())
+        Log.d("numb", b.toString())
+    }
+
+    fun calcNumb(a:Float): Float {
+        var b = 0F
+        var c = 0F
+        while (b < a){
+            c += 1F
+            b += 1/c
+        }
+        return b
     }
 }
